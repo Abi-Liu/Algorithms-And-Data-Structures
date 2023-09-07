c3331d33c98d493b284393dc4b204ee896dccfd5
Abi-Liu <abiliu018@gmail.com>
2023-09-06 17:06:36 -0700

    Group Anagrams Leetcode 49

---
+++ Arrays/Hash/GroupAnagrams/solution.js
@@ -0,0 +1,19 @@
+// Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+var groupAnagrams = function (strs) {
+  // initializes an object for a map to hold array values.
+  const map = {};
+  // for each string in the array, sort the string alphabetically and put it in the map
+  for (const str of strs) {
+    const sorted = str.split("").sort().join("");
+    if (map[sorted]) {
+      map[sorted].push(str);
+    } else {
+      map[sorted] = [str];
+    }
+  }
+  //return the array of values inside the map
+  return Object.values(map);
+};
