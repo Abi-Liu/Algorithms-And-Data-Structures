ef4a778ee6b31115659719c42423e571d5a7b506
Abi Liu <abiliu018@gmail.com>
2023-09-13 00:37:42 -0700

    refactored code and added comments

--- Stacks/ValidParentheses/solution.js
+++ Stacks/ValidParentheses/solution.js
@@ -10,17 +10,20 @@
 
 var isValid = function (s) {
   const arr = [];
+  // we can reference this map to know if we are dealing with a closing or opening bracket
   const map = { "}": "{", ")": "(", "]": "[" };
+  // loop through the string and check to see if s[i] is in the map. If not, that means it is an opening bracket and we can just push it into our stack
+  // if it is in the map, pop off the last element and see if the value is the same as the value stored in the map.
+  // if it is, that means they are a valid pair and we can take them out of the stack, if not return false
   for (let i = 0; i < s.length; i++) {
     if (!map[s[i]]) arr.push(s[i]);
     else {
-      if (arr[arr.length - 1] !== map[s[i]]) {
+      if (arr.pop() !== map[s[i]]) {
         return false;
-      } else {
-        arr.pop();
       }
     }
   }
+  // to cover edge cases like '[' we need to make sure that our stack is empty. If it is we know it is a valid string
   return arr.length === 0 ? true : false;
 };
 
