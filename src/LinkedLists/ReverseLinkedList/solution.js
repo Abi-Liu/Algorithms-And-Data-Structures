c419faaae2c8b65d3f3236053c82858ba8ae7d78
Abi Liu <abiliu018@gmail.com>
2023-10-06 17:31:53 -0700

    reverse linked list iterative solution

---
+++ src/LinkedLists/ReverseLinkedList/solution.js
@@ -0,0 +1,14 @@
+// link: https://leetcode.com/problems/reverse-linked-list/
+
+// iterative solution
+var reverseList = function (head) {
+  let prev = null;
+  let current = head;
+  while (current) {
+    const next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+  return prev;
+};
