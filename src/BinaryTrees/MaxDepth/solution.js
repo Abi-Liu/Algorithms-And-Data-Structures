26c138342ffdc979825c1da259a6d8f6288d1112
Abi Liu <abiliu018@gmail.com>
2023-10-16 20:29:43 -0700

    max depth solution

---
+++ src/BinaryTrees/MaxDepth/solution.js
@@ -0,0 +1,14 @@
+// link:
+
+var maxDepth = function (root) {
+  // base case, we return 0
+  if (!root) {
+    return 0;
+  }
+  // this will continue to recurse until we hit the very bottom of the tree
+  // we then return 0 to the previous call and add 1
+  // it'll keep bubbling up and incrementing by one until we return the final result
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+};
+
+module.exports = maxDepth;
