9c86b863820c8c317b12a7b16c5efb39df1537f1
Abi Liu <abiliu018@gmail.com>
2024-01-31 15:24:18 -0800

    solution missing number

---
+++ src/Hash/MissingNumber/solution.js
@@ -0,0 +1,13 @@
+// link: https://leetcode.com/problems/missing-number/
+
+var missingNumber = function (nums) {
+  // create a set from nums
+  const set = new Set(nums);
+  // loop from 0 to nums.length inclusive and check if the index is in the set.
+  // return the missing index
+  for (let i = 0; i <= nums.length; i++) {
+    if (!set.has(i)) {
+      return i;
+    }
+  }
+};
